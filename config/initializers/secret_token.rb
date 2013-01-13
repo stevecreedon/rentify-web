8f16629924b53341e3e10321593ab03592b4e21b
Arturs Kreipans <arturs.kreipans@gmail.com>
2013-01-13 21:51:10 +0000

    Initial commit
    * rspec
    * factory_girl
    * capybara
    * pg database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentifyWeb::Application.config.secret_token = '95171d265bb3de8e0824e5492c405debadd5cfa85fbdbdc281ab72ae91e4bfa6bebfe60500fc7a89fce0b273de1a1d2a440e30abc96261b98273c02f3eab19e7'
